d8a41962fc8998ceb0700f1a6a5a6b7d0ecf7313
Eikko_ <leo.poilane91@gmail.com>
2025-10-25 20:36:43 +0200

    test deployment fix

--- utils/io.py
+++ utils/io.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import streamlit as st
 
+@st.cache_data
 def load_raw_data():
-    return pd.read_csv("data/paris2024-athletes.csv", sep=";")
+    return pd.read_csv("./data/paris2024-athletes.csv", sep=";", encoding="utf-8")
